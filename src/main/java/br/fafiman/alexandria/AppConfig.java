411472cfe42ad4eff53f04fff2d08996fc8de0d1
Fernando Paschualetto <paschualetto@gmail.com>
2019-06-24 22:28:06 -0300

    Start Spring

---
+++ src/main/java/br/fafiman/alexandria/AppConfig.java
@@ -0,0 +1,10 @@
+package br.fafiman.alexandria;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.fafiman.alexandria")
+public class AppConfig {
+
+}
--- src/main/java/br/fafiman/alexandria/Run.java
+++
@@ -1,9 +0,0 @@
-package br.fafiman.alexandria;
-
-public class Run {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-
-}
